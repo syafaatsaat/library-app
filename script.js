3e6e6d9bbed7361b3afe1332d9c4a28ad72baaf6
syafaatsaat <47024629+syafaatsaat@users.noreply.github.com>
2026-01-13 13:42:10 +0800

    Add book prototype and basic add book function
    Define library array where the function will
    create a new book to add to the array. A unique
    'id' will be generated upon constructor calls
    of the Book prototype.

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(title, author, pages) {
+    if (!new.target) {
+        throw Error("You must the 'new' operator to call the constructor");
+    }
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+function addBookToLibrary(title, author, pages) {
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
+}
