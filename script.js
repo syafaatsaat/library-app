6cc462fd3f2a05d40696454c0d8df16ad0cf695e
Syafaat Saat <47024629+syafaatsaat@users.noreply.github.com>
2026-05-13 16:08:30 +0800

    Add custom form validity to book form

--- script.js
+++ script.js
@@ -122,19 +122,43 @@ const LibrarySystem = (function() {
     };
 
     const checkEntry = () => {
-        const title = document.querySelector("#titleid").value;
-        const author = document.querySelector("#authorid").value;
-        const pages = document.querySelector("#pagesid").value;
+        const titleField = document.getElementById("titleid");
+        const authorField = document.getElementById("authorid");
+        const pagesField = document.getElementById("pagesid");
         const read = document.querySelector("#readstatusid").checked;
-        
-        if (title.length <= 0 
-            || author.length <= 0 
-            || pages.length <= 0) 
-        {
+
+        if (titleField.validity.valueMissing) {
+            titleField.setCustomValidity("The book title must be filled!");
+            titleField.reportValidity();
             return false;
         }
 
-        addBookToLibrary(title, author, pages, read);
+        titleField.setCustomValidity("");
+
+        if (authorField.validity.valueMissing) {
+            authorField.setCustomValidity("The author name must be filled!");
+            authorField.reportValidity();
+            return false;
+        }
+
+        authorField.setCustomValidity("");
+        
+        if (pagesField.validity.valueMissing || 
+            pagesField.validity.rangeUnderflow
+        ) {
+            pagesField.setCustomValidity("The number of pages is too low!");
+            pagesField.reportValidity();
+            return false;
+        }
+
+        pagesField.setCustomValidity("");
+
+        addBookToLibrary(
+            titleField.value, 
+            authorField.value, 
+            pagesField.value, 
+            read
+        );
 
         return true;
     };
